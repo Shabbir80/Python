55c50b81b475ba538b88c0d6d60c079d17f57065
Md  Shabbir Hussain <shabbir526589@gmail.com>
2025-01-12 20:55:55 +0530

    Add files via upload

---
+++ operator and oprands.py
@@ -0,0 +1,44 @@
+# arithmethic operation
+"""print(3%2) #get remiander
+print(2+67)
+print(2**10) #exponential form
+print(8//2) #get quetion
+print(5*4)
+print(14/7)
+print(34-21)"""
+
+#comparsion operater
+print(3==4)
+print(3<=4)
+print(4!=4)
+print(4>=3)
+print(5>=5)
+
+
+#logical operater
+# print(3>5 and 3<6)
+# print(3>5 or 3<6)
+# print(not(3>5 and 3<6))
+
+#Assigment operater
+# a=5
+# a*=6
+# print(a)
+#
+# b = 5
+# b = b*6
+# print(b)
+
+# identical operater
+a = 234
+b = "243"
+print( a is b)
+print( a is not b)
+
+# membership operater
+a = "hello"
+print("h" in a)
+print("y" not in a)
+
+
+
