65b0a5ecfd5fadd2a889d98d43363ca25fd90aba
Md  Shabbir Hussain <shabbir526589@gmail.com>
2025-01-12 20:51:02 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,36 @@
+print("Hello world")
+
+a="fist"
+print(a)
+name=input("enter your name")
+print(name)
+
+age=int(input("enter your age"))
+print(age)
+
+l=float(input("enter float value"))
+print(l)
+
+evalu=eval(input("add or sub or mul etc"))
+print(evalu)
+
+name="asif"
+print(type(name))
+print("hello world")
+
+age=20
+print(type(age))
+
+a="23"
+a=int(a)
+print(type(a))
+
+a = 23
+z = 40
+a,z = z,a
+print(a)
+print(z)
+
+
+
+
